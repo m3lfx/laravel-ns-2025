ee4e7b6f1cce50023cd6acf2c5f7400bdd14dffa
rommel dalisay <rommelmdalisay@gmail.com>
2025-01-14 08:40:01 +0800

    artist create

--- resources/views/artist/create.blade.php
+++ resources/views/artist/create.blade.php
@@ -1,27 +1,29 @@
 @extends('layouts.base')
+
 @section('body')
     <div class="container-lg">
         <form>
             <div class="mb-3">
                 <label for="name" class="form-label">Artist Name</label>
-                <input type="text" class="form-control" id="name" aria-describedby="name">
+                <input type="text" class="form-control" name="name" id="name" aria-describedby="name">
 
             </div>
 
             <div class="mb-3">
                 <label for="country" class="form-label">Artist Country</label>
-                <input type="text" class="form-control" id="country" aria-describedby="country">
+                <input type="text" class="form-control" name="country" id="country" aria-describedby="country">
 
             </div>
 
             <div class="mb-3">
                 <label for="image" class="form-label">Artist Image</label>
-                <input type="text" class="form-control" id="image" aria-describedby="image">
+                <input type="text" class="form-control" name="img_path" id="image" aria-describedby="image">
 
             </div>
 
 
             <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-secondary">Cancel</button>
         </form>
     </div>
 @endsection
